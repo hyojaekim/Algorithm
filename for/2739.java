353b963dde33cab55882724ca3c6a8db02b9b3fa
hyojaekim <41109150+hyojaekim@users.noreply.github.com>
2019-03-24 17:22:18 +0900

    BOJ 2739 : 구구단

---
+++ for/2739.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.valueOf(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<10; i++){
+            sb.append(num + " * " + i + " = " + (num*i) + "\n");
+        }
+        br.close();
+        System.out.println(sb);
+    }
+}
