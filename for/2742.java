1c08242ab2e47c8319cc37c12b5b963102073e13
hyojaekim <41109150+hyojaekim@users.noreply.github.com>
2019-03-24 17:18:46 +0900

    BOJ 2742 : 기찍 N

---
+++ for/2742.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.valueOf(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i=num; i>=1; i--){
+            sb.append(i+"\n");
+        }
+        br.close();
+        System.out.println(sb);
+    }
+}
