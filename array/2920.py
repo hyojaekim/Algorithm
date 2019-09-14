ab8418b9d014d1ce8857a16d154d8d73de4779d1
hyojaekim <hj00686@naver.com>
2019-09-14 16:09:24 +0900

    BOJ 2920 : 음계

---
+++ array/2920.py
@@ -0,0 +1,21 @@
+import sys
+
+value = list(map(int, sys.stdin.readline().split()))
+
+sortValue = value[0] - value[1]
+
+isMixed = False
+
+for index in range(0, (len(value)-1)):
+    subNumber = value[index] - value[index+1]
+
+    if ((subNumber != -1 & subNumber != 1) | subNumber != sortValue):
+        isMixed = True
+        print("mixed")
+        break
+
+if (isMixed == False):
+    if (sortValue == 1):
+        print("descending")
+    if (sortValue == -1):
+        print("ascending")
