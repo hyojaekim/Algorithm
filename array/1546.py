9c13c37668b7fd41c17a1060a8c2093bd02dcc8a
hyojaekim <hj00686@naver.com>
2019-09-14 21:57:37 +0900

    BOJ 1546 : 평균

---
+++ array/1546.py
@@ -0,0 +1,17 @@
+import sys
+
+count = int(sys.stdin.readline())
+score = list(map(int, sys.stdin.readline().split()))
+maxScore = score[0]
+totalScore = 0
+
+for number in score:
+    if(maxScore < number):
+        maxScore = number
+
+
+for index in range(len(score)):
+    score[index] = score[index] / maxScore * 100
+    totalScore += score[index]
+
+print(round(totalScore / count, 2))
