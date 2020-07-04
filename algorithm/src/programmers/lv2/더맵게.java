06deb47f6b329ab7f18264c602f795303008db7a
hyojaekim <hj00686@naver.com>
2020-07-04 21:37:31 +0900

    feat: 더맵게 (PriorityQueue)

--- algorithm/src/programmers/lv2/더맵게.java
+++ algorithm/src/programmers/lv2/더맵게.java
@@ -3,6 +3,7 @@ package programmers.lv2;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class 더맵게 {
     public int solution(int[] data, int K) {
@@ -29,4 +30,20 @@ public class 더맵게 {
         }
         return true;
     }
+
+    public int solution2(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        for (int num : scoville) q.add(num);
+
+        while (true) {
+            if (q.size() == 1 && q.peek() < K) return -1;
+            if (q.peek() > K) return answer;
+            Integer firstNumber = q.poll();
+            Integer secondNumber = q.poll();
+            q.add(firstNumber + (secondNumber * 2));
+            answer++;
+        }
+    }
+
 }
