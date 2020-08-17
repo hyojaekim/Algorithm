7fc004d142ca1d6199d551900d814ec51047f657
hyojaekim <hyojaedev@gmail.com>
2020-08-17 11:25:05 +0900

    feat: 점프와 순간이동

---
+++ algorithm/src/programmers/lv2/점프와_순간이동.java
@@ -0,0 +1,17 @@
+package programmers.lv2;
+
+public class 점프와_순간이동 {
+    public int solution(int n) {
+        int result = 0;
+
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n--;
+                result++;
+            }
+        }
+        return result;
+    }
+}
