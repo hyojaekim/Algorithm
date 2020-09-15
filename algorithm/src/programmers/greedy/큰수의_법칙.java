b3d1d309d39cbb290b7bb47b73e3e7f54dc5bb5f
hyojaekim <hyojaedev@gmail.com>
2020-09-15 21:20:45 +0900

    feat: 큰 수의 법칙 (그리디) - 다른 방법

--- algorithm/src/programmers/greedy/큰수의_법칙.java
+++ algorithm/src/programmers/greedy/큰수의_법칙.java
@@ -22,4 +22,18 @@ public class 큰수의_법칙 {
         }
         return answer;
     }
+
+    public int solution2(int N, int M, int K, int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+        int first = numbers[numbers.length - 1];
+        int second = numbers[numbers.length - 2];
+
+        int firstCnt = ((M / (K + 1)) * K) + (M % (K + 1));
+        int secondCnt = M / (K + 1);
+
+        answer += (first * firstCnt);
+        answer += (second * secondCnt);
+        return answer;
+    }
 }
