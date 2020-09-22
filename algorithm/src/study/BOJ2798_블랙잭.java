4169f2c6934cf0331c04204f6b6fbb7bd04fd0cd
hyojaekim <hyojaedev@gmail.com>
2020-09-22 13:15:16 +0900

    feat: 스터디 - BOJ2798 블랙잭

---
+++ algorithm/src/study/BOJ2798_블랙잭.java
@@ -0,0 +1,50 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ2798_블랙잭 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] nm = br.readLine().split(" ");
+        int n = Integer.parseInt(nm[0]);
+        int m = Integer.parseInt(nm[1]);
+        String[] reqCards = br.readLine().split(" ");
+        int[] cards = new int[n];
+        for (int i = 0; i < reqCards.length; i++) {
+            cards[i] = Integer.parseInt(reqCards[i]);
+        }
+        int result = solution(n, m, cards);
+        System.out.println(result);
+    }
+    
+    public static int solution(int n, int m, int[] cards) {
+        int result = 0;
+        cards = sort(cards);
+        int sum;
+        for (int a = 0; a < n; a++) {
+            if (cards[a] >= m) continue;
+            for (int b = 0; b < n; b++) {
+                if (a == b || cards[a] + cards[b] > m) continue;
+                for (int c = 0; c < n; c++) {
+                    if (c == a || c == b) continue;
+                    sum = cards[a] + cards[b] + cards[c];
+                    if (sum <= m) result = Math.max(sum, result);
+                }
+            }
+        }
+        return result;
+    }
+
+    private static int[] sort(int[] cards) {
+        int[] result = new int[cards.length];
+        Arrays.sort(cards);
+        for (int i = cards.length - 1; i >= 0; i--) {
+            result[cards.length - 1 - i] = cards[i];
+        }
+        return result;
+    }
+}
