17bf38b81e4febf0e294189a2fd9422ec0e0456f
hyojaekim <hyojaedev@gmail.com>
2021-01-04 20:50:05 +0900

    refactor: 조합의 합

--- app/src/test/java/practice/book/coding_interview/chapter12/조합의합.java
+++ app/src/test/java/practice/book/coding_interview/chapter12/조합의합.java
@@ -11,11 +11,11 @@ public class 조합의합 {
 
 	@Test
 	void test() {
-		List<String> result = solution(Arrays.asList(2, 3, 6, 7), 7);
-		Assertions.assertEquals(result.size(), 2);
+		List<List<Integer>> solution = solution(Arrays.asList(2, 3, 6, 7), 7);
+		Assertions.assertEquals(solution.size(), 2);
 	}
 
-	List<String> result;
+	List<List<Integer>> result;
 	List<Integer> candidates;
 
 	/**
@@ -24,24 +24,28 @@ public class 조합의합 {
 	 * @param target 타겟 숫자
 	 * @return 주어진 숫자로 더해서 target이 되는 원소를 나열하라.
 	 */
-	public List<String> solution(List<Integer> candidates, int target) {
+	public List<List<Integer>> solution(List<Integer> candidates, int target) {
 		this.result = new ArrayList<>();
 		this.candidates = candidates;
 
 		for (int i = 0; i < candidates.size(); i++) {
-			dfs(candidates.get(i), i, target, String.valueOf(candidates.get(i)));
+			List<Integer> temp = new ArrayList<>();
+			temp.add(candidates.get(i));
+			dfs(candidates.get(i), i, target, temp);
 		}
 		return result;
 	}
 
-	private void dfs(int sum, int start, int target, String path) {
+	private void dfs(int sum, int start, int target, List<Integer> path) {
 		if (sum > target) return;
 		if (sum == target) {
 			result.add(path);
+			return;
 		}
 
 		for (int i = start; i < candidates.size(); i++) {
-			dfs(sum + candidates.get(i), i, target, path + "," + candidates.get(i));
+			path.add(candidates.get(i));
+			dfs(sum + candidates.get(i), i, target, new ArrayList<>(path));
 		}
 	}
 }
