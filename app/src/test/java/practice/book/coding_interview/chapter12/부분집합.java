748aa98cbfeda7df20471257c849b5efd8558d48
hyojaekim <hyojaedev@gmail.com>
2021-01-05 09:06:14 +0900

    refactor: 부분 집합 알고리즘 개선

--- app/src/test/java/practice/book/coding_interview/chapter12/부분집합.java
+++ app/src/test/java/practice/book/coding_interview/chapter12/부분집합.java
@@ -3,18 +3,15 @@ package practice.book.coding_interview.chapter12;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class 부분집합 {
 	@Test
 	void test() {
 		List<List<Integer>> result = solution(Arrays.asList(1, 2, 3));
-		for (List<Integer> r : result) {
-			for (Integer n : r) {
-				System.out.print(n + " ");
-			}
-			System.out.println();
-		}
 		Assertions.assertEquals(result.size(), 8);
 	}
 
@@ -23,6 +20,7 @@ public class 부분집합 {
 
 	/**
 	 * 사용 알고리즘 : 조합(dfs)
+	 *
 	 * @param numbers 주어진 숫자
 	 * @return 주어진 숫자의 모든 부분 집합을 구하라
 	 */
@@ -34,13 +32,13 @@ public class 부분집합 {
 		return result;
 	}
 
-	private void dfs(int n, int index, Queue<Integer> q) {
-		this.result.add(new LinkedList<>(q));
+	private void dfs(int n, int index, List<Integer> path) {
+		this.result.add(path);
 
 		for (int i = index; i < n; i++) {
-			q.add(numbers.get(i));
-			dfs(n, i + 1, q);
-			q.remove(numbers.get(i));
+			List<Integer> temp = new ArrayList<>(path);
+			temp.add(numbers.get(i));
+			dfs(n, i + 1, temp);
 		}
 	}
 }
