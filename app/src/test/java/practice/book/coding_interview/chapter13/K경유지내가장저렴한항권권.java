cd9857485a058d5e057ffc2fb51641134e3ba498
hyojaekim <hyojaedev@gmail.com>
2021-01-07 21:10:36 +0900

    refactor: 예외처리 추가(K 경유지 내 가장 저렴한 항공권)

--- app/src/test/java/practice/book/coding_interview/chapter13/K경유지내가장저렴한항권권.java
+++ app/src/test/java/practice/book/coding_interview/chapter13/K경유지내가장저렴한항권권.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class K경유지내가장저렴한항권권 {
 	@Test
 	void test() {
-		int solution = solution(3, new int[][]{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}, 0, 2, 0);
+		int solution = findCheapestPrice(3, new int[][]{{0, 1, 100}, {1, 2, 100}, {0, 2, 500}}, 0, 2, 0);
 		System.out.println(solution);
 	}
 
@@ -16,20 +16,20 @@ public class K경유지내가장저렴한항권권 {
 	 * [사용 알고리즘] 다익스트라 알고리즘
 	 *
 	 * @param n     노드 개수
-	 * @param edges [0]:정점 [1]:연결정점 [2]:간선(소요시간)
+	 * @param flights [0]:정점 [1]:연결정점 [2]:간선(소요시간)
 	 * @param src   시작점
 	 * @param dst   도착점
-	 * @param k     경유지 개수
+	 * @param K     경유지 개수
 	 * @return src부터 dst까지 가장 저렴한 가격 (경유지 개수만큼)
 	 */
-	public int solution(int n, int[][] edges, int src, int dst, int k) {
+	public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
 		Map<Integer, List<int[]>> graph = new HashMap<>(); //그래프(인접리스트) 값 세팅
-		for (int[] edge : edges) {
-			graph.computeIfAbsent(edge[0], g -> new ArrayList<>()).add(new int[]{edge[1], edge[2]});
+		for (int[] flight : flights) {
+			graph.computeIfAbsent(flight[0], g -> new ArrayList<>()).add(new int[]{flight[1], flight[2]});
 		}
 
 		PriorityQueue<int[]> minPQ = new PriorityQueue<>(Comparator.comparingInt(o -> o[1])); //힙 [0]:정점 [1]:비용 [2]:경유지개수
-		minPQ.add(new int[]{src, 0, k});
+		minPQ.add(new int[]{src, 0, K});
 
 		while (!minPQ.isEmpty()) {
 			int[] curr = minPQ.poll();
@@ -40,8 +40,10 @@ public class K경유지내가장저렴한항권권 {
 			}
 
 			if (currK >= 0) { //요청하는 경유지 개수보다 작은 경우 힙에 넣음
-				for (int[] nei : graph.get(currVertex)) {
-					minPQ.offer(new int[]{nei[0], nei[1] + currCost, currK - 1});
+				if (graph.containsKey(currVertex)) {
+					for (int[] nei : graph.get(currVertex)) {
+						minPQ.offer(new int[]{nei[0], nei[1] + currCost, currK - 1});
+					}
 				}
 			}
 		}
