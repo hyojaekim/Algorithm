18875adad7b1b27be8973ac6b5ac974cb853bd94
hyojaekim <hyojaedev@gmail.com>
2021-01-15 20:08:49 +0900

    feat: [3차] 파일명 정렬

---
+++ app/src/test/java/practice/programmers/lv2/파일명정렬_3차.java
@@ -0,0 +1,74 @@
+package practice.programmers.lv2;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class 파일명정렬_3차 {
+
+	@Test
+	void test() {
+		String[] solution = solution(new String[]{"img12", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"});
+		System.out.println(Arrays.toString(solution));
+	}
+
+	public String[] solution(String[] files) {
+		String[] result = new String[files.length];
+		List<File> convertedFiles = init(files);
+		convertedFiles.sort(Comparator.comparing((File o) -> o.head.toLowerCase()).thenComparingInt(o -> Integer.parseInt(o.number)).thenComparingInt(o -> o.index));
+		for (int i = 0; i < result.length; i++) {
+			result[i] = convertedFiles.get(i).toString();
+
+		}
+		return result;
+	}
+
+	private List<File> init(String[] files) {
+		List<File> result = new ArrayList<>();
+		for (int i = 0; i < files.length; i++) {
+			result.add(new File(files[i], i));
+		}
+		return result;
+	}
+
+	class File {
+		String head;
+		String number;
+		String tail;
+		int index;
+
+		public File(String file, int index) {
+			int numberStartIndex = getNumberStartIndex(file);
+			int numberEndIndex = getNumberEndIndex(file, numberStartIndex);
+
+			this.head = file.substring(0, numberStartIndex);
+			this.number = file.substring(numberStartIndex, numberEndIndex);
+			this.tail = numberEndIndex == file.length() ? "" : file.substring(numberEndIndex);
+			this.index = index;
+		}
+
+		private int getNumberStartIndex(String file) {
+			for (int i = 0; i < file.length(); i++) {
+				char c = file.charAt(i);
+				if (c >= '0' && c <= '9') return i;
+			}
+			return 0;
+		}
+
+		private int getNumberEndIndex(String file, int start) {
+			for (int i = start; i < file.length(); i++) {
+				char c = file.charAt(i);
+				if (!(c >= '0' && c <= '9')) return i;
+			}
+			return file.length();
+		}
+
+		@Override
+		public String toString() {
+			return this.head + this.number + this.tail;
+		}
+	}
+}
