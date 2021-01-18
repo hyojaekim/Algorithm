fed9039a2bb9500728477f9a522f791c59509a4a
hyojaekim <hyojaedev@gmail.com>
2021-01-18 17:44:58 +0900

    feat: [3차] 방금그곡

--- app/src/test/java/practice/programmers/lv2/방금그곡_3차.java
+++ app/src/test/java/practice/programmers/lv2/방금그곡_3차.java
@@ -1,69 +1,81 @@
 package practice.programmers.lv2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class 방금그곡_3차 {
 
 	@Test
 	void test() {
-//		String result = solution("ABCDEFG", new String[]{"12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"});
-		String result = solution("ABC", new String[]{"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"});
-		System.out.println(result);
+		String solution = solution("CC#BCC#BCC#BCC#B", new String[]{"03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"});
+		Assertions.assertEquals(solution, "FOO");
 	}
 
-	Map<String, String> map;
+	/**
+	 * [문제] https://programmers.co.kr/learn/courses/30/lessons/17683
+	 * [분류] 구현
+	 *
+	 * @param m 주어진 악보
+	 * @param musicinfos 0:시작시간, 1:끝난시간, 2:제목, 3:악보
+	 * @return 조건에 일치하는 음악의 제목을 반환하라.
+	 */
 	public String solution(String m, String[] musicinfos) {
-		map = new HashMap<>();
-		map.put("C#", "*"); map.put("D#", "!"); map.put("F#", "@"); map.put("G#", "$"); map.put("A#", "^");
-		for (String key : map.keySet()) {
-			if (!m.contains(key)) continue;
-			m = m.replace(key, map.get(key));
-		}
-
-		List<Music> musicList = init(musicinfos);
+		m = getScore(m);
+		List<Music> musicList = getMusicList(musicinfos);
 		List<Music> result = new ArrayList<>();
+
 		for (Music music : musicList) {
-			if (music.score.contains(m)) {
+			if (music.time < m.length()) continue;
+			if (music.possible(m)){
 				result.add(music);
 			}
 		}
+
 		if (result.size() == 0) return "(None)";
+
 		result.sort((o1, o2) -> {
 			int n = o2.time - o1.time;
-			if (n == 0) return o2.index - o1.index;
-			return n;
+			if (n != 0) return n;
+			return o1.index - o2.index;
 		});
 		return result.get(0).title;
 	}
 
-	private List<Music> init(String[] musicinfos) {
-		List<Music> result = new ArrayList<>();
-		for (int i = 0; i < musicinfos.length; i++) {
-			String[] musicinfo = musicinfos[i].split(",");
-			result.add(new Music(musicinfo[0], musicinfo[1], musicinfo[2], musicinfo[3], i));
+	private String getScore(String s) {
+		char[] result = s.toCharArray();
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) != '#') continue;
+			result[i - 1] = Character.toLowerCase(result[i - 1]);
 		}
-		return result;
+		return new String(result).replace("#", "");
+	}
+
+	private List<Music> getMusicList(String[] musicinfos) {
+		List<Music> musicList = new ArrayList<>();
+		for (int i = 0; i < musicinfos.length; i++) {
+			String[] temp = musicinfos[i].split(",");
+			musicList.add(new Music(temp[0], temp[1], temp[2], getScore(temp[3]), i));
+		}
+		return musicList;
 	}
 
 	class Music {
-		int time; //재생 시간
-		String title; //제목
-		String score; //악보
-		int index; //요청 순서
+		int time;
+		String title;
+		String score;
+		int index;
 
 		public Music(String startTime, String endTime, String title, String score, int index) {
-			this.time = calculateTime(startTime, endTime);
+			this.time = getTime(startTime, endTime);
 			this.title = title;
-			this.score = setScore(score);
+			this.score = score;
 			this.index = index;
 		}
 
-		private int calculateTime(String startTime, String endTime) {
+		private int getTime(String startTime, String endTime) {
 			String[] convertedStartTime = startTime.split(":");
 			int startHour = Integer.parseInt(convertedStartTime[0]);
 			int startMinute = Integer.parseInt(convertedStartTime[1]);
@@ -72,31 +84,24 @@ public class 방금그곡_3차 {
 			int endHour = Integer.parseInt(convertedEndTime[0]);
 			int endMinute = Integer.parseInt(convertedEndTime[1]);
 
-			if (startHour == endHour) {
-				return endMinute - startMinute;
-			} else if (endMinute > startMinute) {
-				return ((endHour - startHour) * 60) + (endMinute - startMinute);
-			} else if (endMinute < startMinute) {
-				return ((endHour - startHour) * 60) - (startMinute - endMinute);
-			}
-			return 0;
+			if (startHour == endHour) return endMinute - startMinute;
+			else if (startMinute > endMinute) return ((endHour - startHour) * 60) - (startMinute - endMinute);
+			else if (startMinute < endMinute) return ((endHour - startHour) * 60) + (endMinute - startMinute);
+			return (endHour - startHour) * 60;
 		}
 
-		private String setScore(String score) {
-			for (String key : map.keySet()) {
-				if (!score.contains(key)) continue;
-				score = score.replace(key, map.get(key));
+		public boolean possible(String m) {
+			StringBuilder sb = new StringBuilder(score);
+			if (time < score.length()) sb = new StringBuilder(score.substring(0, time));
+			else if (time > score.length()) {
+				for (int i = 1; i < time / score.length(); i++) {
+					sb.append(score);
+				}
+				int n = time % score.length();
+				if (n != 0) sb.append(score, 0, n);
 			}
-			if (this.time == score.length()) return score;
-			if (this.time < score.length()) return score.substring(0, this.time);
-			int n = this.time / score.length();
-			for (int i = 0; i < n; i++) {
-				score += score;
-			}
-			String m = (this.time % score.length() == 0) ? "" : score.substring(0, this.time % score.length());
-			return score + m;
+			String s = sb.toString();
+			return s.contains(m);
 		}
 	}
-
 }
-
