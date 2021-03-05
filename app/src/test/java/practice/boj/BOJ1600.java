3c2ef14dc8ed6718aecd7af791132f7d0b93b33a
hyojaekim <hyojaedev@gmail.com>
2021-03-05 16:18:45 +0900

    feat: BOJ1600

--- app/src/test/java/practice/boj/BOJ1600.java
+++ app/src/test/java/practice/boj/BOJ1600.java
@@ -3,7 +3,6 @@ package practice.boj;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -11,7 +10,7 @@ public class BOJ1600 {
 
 	@Test
 	void name() {
-		int solution = solution(1, 4, 4, new int[][]{
+		int solution = bfs(1, 4, 4, new int[][]{
 						{0, 0, 0, 0},
 						{1, 0, 0, 0},
 						{0, 0, 1, 0},
@@ -21,72 +20,64 @@ public class BOJ1600 {
 		Assertions.assertEquals(solution, 4);
 	}
 
-	int[] ndx = {-2, 2, 0, 0};
-	int[] ndy = {0, 0, -2, 2};
-	int[] ndxx = {0, 0, -1, 1};
-	int[] ndyy = {-1, 1, 0, 0};
+	@Test
+	void test2() {
+		int solution = bfs(1, 4, 4, new int[][]{
+						{0, 1, 1, 1},
+						{0, 0, 1, 1},
+						{1, 0, 1, 1},
+						{1, 1, 1, 0},
+		});
+
+		Assertions.assertEquals(solution, 4);
+	}
 
 	int[] dx = {0, -1, 0, 1};
 	int[] dy = {1, 0, -1, 0};
+	int[] ndx = {1, 2, 2, 1, -1, -2, -2, -1};
+	int[] ndy = {2, 1, -1, -2, -2, -1, 1, 2};
 
-	public int solution(int k, int w, int h, int[][] board) {
-		int[][] nightBoard = init(w, h);
-		int[][] dist = init(w, h);
-
-		return bfs(k, board, dist, nightBoard, w, h);
-	}
-
-	private int bfs(int k, int[][] board, int[][] dist, int[][] nightBoard, int w, int h) {
+	public int bfs(int k, int w, int h, int[][] board) {
+		boolean[][][] visited = new boolean[h][w][k + 1]; //(x, y) -> 말 이동 횟수 만큼 방문했는가?
 		Queue<Point> q = new LinkedList<>();
-		q.offer(new Point(0, 0));
-		dist[0][0] = 0;
-		nightBoard[0][0] = 0;
+		q.offer(new Point(0, 0, k, 0));
+		visited[0][0][0] = true;
 
 		while (!q.isEmpty()) {
 			Point cur = q.poll();
-			if (cur.x == w - 1 && cur.y == h - 1) break;
+			if (cur.x == h - 1 && cur.y == w - 1) return cur.cnt;
 
-			//나이트 4방향
-			for (int i = 0; i < 4; i++) {
-				int nx = cur.x + ndx[i] + ndxx[i];
-				int ny = cur.y + ndy[i] + ndyy[i];
-
-				if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
-				if (dist[nx][ny] != -1 || nightBoard[cur.x][cur.y] == k) continue; //k만큼 썼으면 pass
-				dist[nx][ny] = dist[cur.x][cur.y] + 1;
-				nightBoard[nx][ny] = nightBoard[cur.x][cur.y] + 1;
-				q.offer(new Point(nx, ny));
-			}
-
-			//일반 4방향
-			for (int i = 0; i < 4; i++) {
+			for (int i = 0; i < 4; i++) { //상하좌우 이동
 				int nx = cur.x + dx[i];
 				int ny = cur.y + dy[i];
 
-				if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
-				if (dist[nx][ny] != -1 || board[nx][ny] == 1) continue; //장애물도 확인
-				dist[nx][ny] = dist[cur.x][cur.y] + 1;
-				nightBoard[nx][ny] = nightBoard[cur.x][cur.y];
-				q.offer(new Point(nx, ny));
+				if (nx < 0 || ny < 0 || nx >= h || ny >= w || board[nx][ny] == 1 || visited[nx][ny][cur.k]) continue;
+				visited[nx][ny][cur.k] = true;
+				q.offer(new Point(nx, ny, cur.k, cur.cnt + 1)); //말 이동 횟수 변동 x, 이동 횟수 + 1
+			}
+
+			if (cur.k <= 0) continue;
+
+			for (int i = 0; i < 8; i++) {
+				int nx = cur.x + ndx[i];
+				int ny = cur.y + ndy[i];
+
+				if (nx < 0 || ny < 0 || nx >= h || ny >= w || board[nx][ny] == 1 || visited[nx][ny][cur.k]) continue;
+				visited[nx][ny][cur.k - 1] = true;
+				q.offer(new Point(nx, ny, cur.k - 1, cur.cnt + 1)); //말 이동 횟수 -1, 이동 횟수 + 1
 			}
 		}
-		return dist[h - 1][w - 1];
+		return -1;
 	}
 
-	private int[][] init(int w, int h) {
-		int[][] result = new int[h][w];
-		for (int i = 0; i < w; i++) {
-			Arrays.fill(result[i], -1);
-		}
-		return result;
-	}
+	static class Point {
+		int x, y, k, cnt;
 
-	class Point {
-		int x, y;
-
-		public Point(int x, int y) {
+		public Point(int x, int y, int k, int cnt) {
 			this.x = x;
 			this.y = y;
+			this.k = k;
+			this.cnt = cnt;
 		}
 	}
 }
