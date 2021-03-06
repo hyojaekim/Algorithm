ba3df8148cf5f88dba8073f38d5e4c104393d59b
hyojaekim <hyojaedev@gmail.com>
2021-03-06 23:05:35 +0900

    style: BOJ1600
    - 주석 추가

--- app/src/test/java/practice/boj/BOJ1600.java
+++ app/src/test/java/practice/boj/BOJ1600.java
@@ -37,6 +37,24 @@ public class BOJ1600 {
 	int[] ndx = {1, 2, 2, 1, -1, -2, -2, -1};
 	int[] ndy = {2, 1, -1, -2, -2, -1, 1, 2};
 
+	/**
+	 * [문제] https://www.acmicpc.net/problem/1600
+	 * [분류] 그래프, BFS
+	 * [풀이]
+	 * 1. visted[x][y][k] -> (x, y)에 k번 말 이동했을 때, 방문했는지 여부 판별
+	 * 2. 상하좌우(4방향) -> 말 이동 횟수 변동 x, 전체 이동 횟수 +1
+	 * 3. Night(8방향) -> 말 이동 횟수 -1, 전체 이동 횟수 + 1
+	 * 4. 공통 -> 범위 체크, 장애물 체크, 방문 여부 확인
+	 *
+	 * [삽질]
+	 * 1. 다른 곳에서 먼저 도착했을 때, 이동할 수 없었음
+	 * 2. h, w 가로 세로 반대로 해서 찾는데 시간이 걸림
+	 * @param k 말처럼 이동할 수 있는 횟수
+	 * @param w 가로 횟수
+	 * @param h 세로 횟수
+	 * @param board map
+	 * @return 최소 이동 횟수
+	 */
 	public int bfs(int k, int w, int h, int[][] board) {
 		boolean[][][] visited = new boolean[h][w][k + 1]; //(x, y) -> 말 이동 횟수 만큼 방문했는가?
 		Queue<Point> q = new LinkedList<>();
