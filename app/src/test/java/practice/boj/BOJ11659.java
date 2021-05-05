f15c45c6667e49e62366ed0e03dfd583e187696b
hyojaekim <hyojaedev@gmail.com>
2021-05-05 13:55:34 +0900

    feat: BOJ11659

---
+++ app/src/test/java/practice/boj/BOJ11659.java
@@ -0,0 +1,35 @@
+package practice.boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ11659 {
+
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] split = br.readLine().split(" ");
+		int n = Integer.parseInt(split[0]);
+		int m = Integer.parseInt(split[1]);
+		int[] numbers = new int[n];
+		split = br.readLine().split(" ");
+		numbers[0] = Integer.parseInt(split[0]);
+		for (int i = 1; i < n; i++) {
+			numbers[i] = Integer.parseInt(split[i]) + numbers[i - 1];
+		}
+
+		for (int k = 0; k < m; k++) {
+			split = br.readLine().split(" ");
+			int i = Integer.parseInt(split[0]) - 1;
+			int j = Integer.parseInt(split[1]) - 1;
+
+			if (i == 0) {
+				System.out.println(numbers[j]);
+			} else {
+				System.out.println(numbers[j] - numbers[i - 1]);
+			}
+		}
+	}
+}
